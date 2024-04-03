dfd1fc8500a7f09a1d162762de0136e1a68a4cb1
Juan Diego Leon <jdleon199@gmail.com>
2024-04-02 22:35:19 -0500

    segunda actualizacion

--- Semana03Sesion01/d19423/script.js
+++ Semana03Sesion01/d19423/script.js
@@ -1,4 +1,14 @@
 let boton1 = document.getElementById("btnTercerBoton");
         boton1.addEventListener("click",function(){
             alert("Hola desde el tercer boton")
-        });
+});
+
+let edad = 40;
+{
+        let edad = 55;
+}
+
+console.log(edad);
+
+const PI = 3.14159;
+const A = 3.14;
