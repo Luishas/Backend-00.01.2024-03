408983ee3dff6a2b9b6887fe886e0e14cc882074
Juan Diego Leon <jdleon199@gmail.com>
2024-04-04 22:01:51 -0500

    Semana 3

--- Semana03Sesion01/d19423/script.js
+++ Semana03Sesion01/d19423/script.js
@@ -48,3 +48,85 @@ console.log(isNaN(x));
 // Tipos de datos booleanos
 // OR  AND 
 
+//let mes = fechaNacimiento.substring(5,7);
+//console.log(mes)
+
+//let dia = fechaNacimiento.substring(8,12);
+//console.log(dia)
+
+const d = new Date("October 13, 2024 11:10:00");
+console.log(d)
+
+console.log(d.getFullYear())
+console.log(d.getMonth()+1)
+
+console.log(d.toISOString());
+
+let number = 5;
+
+console.log(number=="5")
+console.log(number==="5")
+let age = 15;
+let voteable = (age < 18) ? "Too young": "Old enough";
+console.log(voteable)
+
+// ARRAY
+
+let hobbies = ["musica","juegos","deportes"];
+let otroHobbie = [true,33,12.33,new Date(),"Hola", hobbies];
+
+console.log(hobbies)
+console.log(otroHobbie)
+
+let objCarro={
+    color: "Blanco",
+    marca: "BMW",
+    combustible: "Gasolina",
+    transmision: true,
+    nroPuertas: 5,
+    cilindraje: 1899.9999
+}
+
+const fruits = ["banana","naranja","mandarina","apple"];
+let size = fruits.length;
+
+console.log(objCarro)
+console.log(fruits.toString())
+fruits.push("mango");
+console.log(fruits.toString())
+fruits.pop();
+console.log(fruits.toString())
+console.log(fruits.join(" * "));
+fruits.shift();
+console.log(fruits.toString())
+
+const myGirls = ["cecil","lone"];
+const myBoys = ["emil","tobias","linus"];
+
+const myChildren = myGirls.concat(myBoys);
+console.log(myChildren)
+
+for (let index = 0;index < myChildren.length; index++){
+    const element = myChildren[index];
+    console.log(element)
+}
+
+console.log(objCarro.marca)
+
+let hora = new Date().getHours();
+
+if (hora < 18){
+    greeting = "Good day";
+} else {
+    greeting = "Good evening";
+}
+
+console.log(greeting)
+
+let i=0
+while(i<10){
+    let text = '';
+    text += "The number is " +i;
+    console.log(text)
+    i++;
+}
